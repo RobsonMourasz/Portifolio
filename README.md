730be78b6aae2ede603b02042cd9867dc5325399
RobsonMourasz <robsonic10@gmail.com>
2024-12-20 17:58:45 -0300

    Refazendo meu Portifólio

--- README.md
+++
@@ -1,4 +0,0 @@
-<h1>Projeto de portifólio </h1>
-<p>Projeto feito para fins de estudo</p>
-<img src="assets/img/Untitled.png" alt="">
-<img src="htdocs/assets/Untitled.png">
--- htdocs/assets/js/acoes.js
+++
@@ -1,9 +0,0 @@
-const html = document.getElementsByTagName("html")[0];
-document.getElementById("btnModo").addEventListener("click", function () {
-    html.classList.toggle("light")
-    if(html.classList.contains("light")){
-        document.getElementById("btnModo").innerHTML = '<img src="assets/img/icons/temaClaro.svg" alt="Tema Claro"></img>';
-    }else{
-        document.getElementById("btnModo").innerHTML = '<img src="assets/img/icons/temaEscuro.svg" alt="Tema Claro"></img>';
-    }
-})
